18c28207f6c4b163909fc6d33e4af8e2af1aa852
JamwalTechnology <jamwaltechnology@gmail.com>
2026-09-25 04:40:24 +0530

    topbar

--- resources/views/front/layouts/top-bar.blade.php
+++ resources/views/front/layouts/top-bar.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 d-flex justify-content-lg-start justify-content-center">
-                <span class="phone-top d-flex align-items-center"> <a href="tel:123-456-7890"><i class="icon cps cp-phone align-middle px-1"></i> <span class="label">{{$setting->phone}}</span></a></span>
+                <span class="phone-top d-flex align-items-center"> <a href="tel:123-456-7890"><i class="icon cps cp-phone align-middle px-1"></i> <span class="label">{{$setting->phone ?? "+917006291696"}}</span></a></span>
                 <span class="language-dropdown top-dropdown d-flex align-items-center">
                     <i class="cps cp-language"></i>
                     <ul class="nav">
